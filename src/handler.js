96654e18ccbd5c4001ce1e8a307df82ac4634d96
Rifkymoll <Rifkymaulana123@gmail.com>
2022-06-07 10:21:07 +0700

    edit notes

--- src/handler.js
+++ src/handler.js
@@ -63,4 +63,42 @@ const getNoteByIdHandler = (request, h) => {
   return response;
 }
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Note updated successfuly',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Failed to update note. Id not found!'
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { 
+  addNoteHandler, 
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,  
+};
